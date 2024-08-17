1f5c5425f01d146e85939928e8baf1946d095140
Ruchir Kasineni <rkasineni@gmail.com>
2024-08-16 21:30:35 -0400

    Adding model directory

--- analysis/model.py
+++ analysis/model.py
@@ -1,12 +1,10 @@
 ### 3 Year Average of Every WR
 ### 5 Year Trailing Stats of Input WR
 ### Contracts for Every WR
-from etl.load import load_dataframes
+import pandas as pd
 
 def create_model():
-    wr_df, contract_df = load_dataframes()
-
-    print(wr_df)
-    print(contract_df)
+    wr_dataframe = pd.read_csv('data/outputs/contract_data_aggregated.csv')
+    contract_dataframe = pd.read_csv('data/outputs/wide_receiver_data_aggregated.csv')
 
 print(create_model())
